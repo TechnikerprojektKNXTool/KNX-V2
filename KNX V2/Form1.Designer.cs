a567275622b132df13de4df3f6e2796df9188718
Nyminus <drestbetz@googlemail.com>
2025-11-10 14:21:37 +0100

    Button reihenfolge abgeändert

--- KNX V2/Form1.Designer.cs
+++ KNX V2/Form1.Designer.cs
@@ -30,17 +30,17 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -60,23 +60,23 @@
             this.textBox2.TabIndex = 29;
             this.textBox2.Text = "Raum";
             // 
-            // button6
+            // button2
             // 
-            this.button6.Location = new System.Drawing.Point(412, 50);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(171, 23);
-            this.button6.TabIndex = 32;
-            this.button6.Text = "ausgew. Raum kopieren";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button2.Location = new System.Drawing.Point(412, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 23);
+            this.button2.TabIndex = 32;
+            this.button2.Text = "ausgew. Raum kopieren";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // button1
             // 
-            this.button5.Location = new System.Drawing.Point(412, 21);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(171, 23);
-            this.button5.TabIndex = 31;
-            this.button5.Text = "neuen Raum hinzufügen";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(412, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 23);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "neuen Raum hinzufügen";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // listView1
             // 
@@ -111,41 +111,41 @@
             this.columnHeader4.Text = "Index";
             this.columnHeader4.Width = 1;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(412, 118);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(171, 23);
-            this.button1.TabIndex = 34;
-            this.button1.Text = "Raum konfigurieren";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(412, 147);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(171, 23);
-            this.button2.TabIndex = 35;
-            this.button2.Text = "Eintrag bearbeiten";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(412, 176);
+            this.button3.Location = new System.Drawing.Point(412, 118);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(171, 23);
-            this.button3.TabIndex = 36;
-            this.button3.Text = "Eintrag löschen";
+            this.button3.TabIndex = 34;
+            this.button3.Text = "Raum konfigurieren";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(399, 390);
+            this.button4.Location = new System.Drawing.Point(412, 147);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(110, 23);
-            this.button4.TabIndex = 37;
-            this.button4.Text = "Speichern";
+            this.button4.Size = new System.Drawing.Size(171, 23);
+            this.button4.TabIndex = 35;
+            this.button4.Text = "Eintrag bearbeiten";
             this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(412, 176);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(171, 23);
+            this.button5.TabIndex = 36;
+            this.button5.Text = "Eintrag löschen";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(399, 390);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 23);
+            this.button6.TabIndex = 37;
+            this.button6.Text = "Speichern";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
@@ -162,13 +162,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button7);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listView1);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.textBox2);
             this.Name = "Form1";
@@ -182,17 +182,17 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.ColumnHeader columnHeader4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
     }
 }
--- KNX V2/Form2.Designer.cs
+++ KNX V2/Form2.Designer.cs
@@ -37,9 +37,9 @@
             this.tabPage6 = new System.Windows.Forms.TabPage();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.comboBox4 = new System.Windows.Forms.ComboBox();
-            this.button4 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
@@ -136,41 +136,41 @@
             this.comboBox4.Size = new System.Drawing.Size(181, 24);
             this.comboBox4.TabIndex = 34;
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(12, 960);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(110, 23);
-            this.button4.TabIndex = 38;
-            this.button4.Text = "Zurück";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(757, 960);
+            this.button1.Location = new System.Drawing.Point(12, 960);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 23);
-            this.button1.TabIndex = 39;
-            this.button1.Text = "Speichern";
+            this.button1.TabIndex = 38;
+            this.button1.Text = "Zurück";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1802, 960);
+            this.button2.Location = new System.Drawing.Point(757, 960);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(110, 23);
-            this.button2.TabIndex = 40;
-            this.button2.Text = "Weiter";
+            this.button2.TabIndex = 39;
+            this.button2.Text = "Speichern";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1802, 960);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 23);
+            this.button3.TabIndex = 40;
+            this.button3.Text = "Weiter";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form2";
             this.Text = "Form2";
@@ -192,8 +192,8 @@
         private System.Windows.Forms.TabPage tabPage6;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.ComboBox comboBox4;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
