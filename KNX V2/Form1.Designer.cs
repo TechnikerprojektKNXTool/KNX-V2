53016d8db9f75488ecd56612e636944fee3a2418
Sabi234 <sabrina.hahn@hotmail.de>
2026-02-12 11:49:49 +0100

    Speichern und speichern unter programmiert, Sortierfunktion nachgebessert, Raum kopieren Dialog hinzugefügt, vor Excel-Export Raumliste sortieren

--- KNX V2/Form1.Designer.cs
+++ KNX V2/Form1.Designer.cs
@@ -111,8 +111,8 @@
             this.listView1.TabIndex = 33;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
-            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // columnHeader1
             // 
@@ -257,6 +257,7 @@
             this.button9.TabIndex = 196;
             this.button9.Text = "Speichern unter";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // label5
             // 
@@ -316,8 +317,7 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;           //hier wurde autoscale von Font auf None geändert!!!!****************************************************************************
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(1924, 1055);
             this.Controls.Add(this.listView2);
             this.Controls.Add(this.label5);
